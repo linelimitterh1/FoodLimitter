0b2bd822caff9ce3002fa6208b3d0549704c8290
Mt-Fuji-wu <s226225@wakayama-u.ac.jp>
2019-07-17 13:07:22 +0900

    firebasefunctionの使用直前

--- FoodLimitter/routes/input.js
+++ FoodLimitter/routes/input.js
@@ -1,7 +1,12 @@
+const admin = require('firebase-admin');
+const funcitions = require('firebase-functions');
 var express =require('express');
 var router = express.Router();
 
-var img = 
+//setup firestore
+admin.initializeApp(funcitions.config().firebase);
+var db = admin.firestore();
+
 
 router.get('/',(req,res,next) =>{
     var data = {
@@ -10,5 +15,17 @@ router.get('/',(req,res,next) =>{
     }
     res.render('input',data);
 });
+//Post new users
+router.post('/',(req,res,next) =>{
+    var newData =req.body;
+    var docRef = db.collection('users').doc(newData.name);
+    docRef.set(newData).then(ref =>{
+        console.log('success');
+        res.send('success');
+    }).catch(function(error){
+        console.log(error);
+        next(error);
+    });
+})
 
 module.exports = router;
--- FoodLimitter/routes/stocklist.js
+++ FoodLimitter/routes/stocklist.js
@@ -1,9 +1,13 @@
 //stocklistのjs
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
 var express =require('express');
 var router = express.Router();
-
+//setup firestore
+admin.initalizeApp(functions.config().firebase);
+var db = admin.firestore();
+//日付の取得
 require('date-utils');
-
 var dt = new Date();
 var now_date = dt.toFormat("YYYY年MM月DD日(DDD)")
 
@@ -12,7 +16,18 @@ router.get('/',(req,res,next) =>{
 		title: "登録一覧",
 		now_date: now_date
 	}
-	res.render('stocklist',data);
+	db.collection('users').get()
+		.then((snapshot) => {
+			var users = new Array();
+			snapshot.forEach((doc) => {
+				users.push(doc.data());
+			});
+			res.json(users);
+			res.render('stocklist',data);
+		})
+		.catch((err) => {
+			next(err);
+		});
 	});
 
 
